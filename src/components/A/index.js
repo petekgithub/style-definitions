d3c7722e29eb17889c4aa31317eca32256973360
Petek Savaş Hamitbeyli <38165351+petekgithub@users.noreply.github.com>
2022-04-22 14:07:38 +0300

    module css

---
+++ src/components/A/index.js
@@ -0,0 +1,11 @@
+import style from './style.module.css'
+
+console.log(style)
+
+function A(){
+  return (
+    <div className={style.title}>A</div>
+  )
+}
+
+export default A
---
+++ src/components/B/index.js
@@ -0,0 +1,11 @@
+import style from './style.module.css'
+
+console.log(style)
+
+function B(){
+  return (
+    <div className={style.title}>B</div>
+  )
+}
+
+export default B
